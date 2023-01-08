1ddbbf3763b608ed806d1e288b1623a2332f5c31
Everson Ortega <everson_helltram@hotmail.com>
2023-01-08 10:56:52 -0300

    creating-folder-structures

---
+++ src/main/java/com/ever/audit/example/using/envers/AuditExampleUsingEnversApplication.java
@@ -0,0 +1,13 @@
+package com.ever.audit.example.using.envers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditExampleUsingEnversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditExampleUsingEnversApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ever/audit/example/using/envers/AuditExampleUsingEnversApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ever.audit.example.using.envers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditExampleUsingEnversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
